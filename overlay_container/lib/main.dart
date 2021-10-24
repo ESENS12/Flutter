d13723c55c9a4d32f2305c9ca8a009cc1f2edf79
ESENS12 <ghdldid12@gmail.com>
2021-10-24 14:37:59 +0900

    Update TIL

---
+++ overlay_container/lib/main.dart
@@ -0,0 +1,97 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      alignment: Alignment.center,
+      children: <Widget>[
+        Positioned(
+          top: 0,
+          child: Container(
+            color: Colors.deepPurple,
+            width: MediaQuery.of(context).size.width,
+            height: MediaQuery.of(context).size.height * .35,
+          ),
+        ),
+        Positioned(
+          //현재 해상도의 height에서 25% 위치에 top
+          top: MediaQuery.of(context).size.height * .25,
+          left: 15,
+          right: 15,
+          child: Card(
+            elevation: 8,
+            color: Colors.white,
+            shape:
+            RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+            child: Container(
+              width: MediaQuery.of(context).size.height * .90,
+              height: 220,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: <Widget>[
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: const <Widget>[
+                      Icon(
+                        Icons.scanner,
+                        color: Colors.deepPurple,
+                        size: 45,
+                      ),
+                      Text("SCAN QR")
+                    ],
+                  ),
+                  Container(
+                    height: 100,
+                    width: 2,
+                    color: Colors.deepPurple,
+                  ),
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: const <Widget>[
+                      Icon(
+                        Icons.bluetooth,
+                        color: Colors.deepPurple,
+                        size: 45,
+                      ),
+                      Text("BEACON")
+                    ],
+                  )
+                ],
+              ),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
