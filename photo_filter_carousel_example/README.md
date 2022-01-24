c8d4cb792f49fac6786bd212a5fde228f527e687
ESENS12 <ghdldid12@gmail.com>
2022-01-24 17:02:18 +0900

    Update TIL

---
+++ photo_filter_carousel_example/README.md
@@ -0,0 +1,4 @@
+# Photo Filter Carousel Example
+
+# References
+ - https://docs.flutter.dev/cookbook/effects/photo-filter-carousel
---
+++ photo_filter_carousel_example/android/app/src/main/kotlin/kr/esens/photo_filter_carousel_example/MainActivity.kt
@@ -0,0 +1,6 @@
+package kr.esens.photo_filter_carousel_example
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
