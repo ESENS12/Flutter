8a01af7e8ce2bf06a0acc8c8c7db541ce8ebc383
ESENS12 <ghdldid12@gmail.com>
2022-03-28 17:30:54 +0900

    Update TIL

---
+++ FlutterPermissionHandler/README.md
@@ -0,0 +1,5 @@
+# flutter_permission_handler
+ - A Simple Permission request using Permission Handler (for Android)
+
+# References
+ - https://pub.dev/packages/permission_handler
---
+++ FlutterPermissionHandler/lib/main.dart
@@ -0,0 +1,94 @@
+
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:permission_handler/permission_handler.dart';
+import 'dart:io';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  final Permission _permission = Permission.storage;
+  PermissionStatus _permissionStatus = PermissionStatus.denied;
+
+  @override
+  void initState() {
+    _listenForPermissionStatus();
+  }
+
+  void _listenForPermissionStatus() async {
+    final status = await _permission.status;
+    setState(() => _permissionStatus = status);
+  }
+
+  void checkServiceStatus(BuildContext context,
+      PermissionWithService permission) async {
+    ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+      content: Text((await permission.serviceStatus).toString()),
+    ));
+  }
+
+  Future<void> requestPermission(Permission permission) async {
+
+    final status = await permission.request();
+    setState(() {
+      // print(status);
+      _permissionStatus = status;
+      // print(_permissionStatus);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'You have pushed the permission state is : $_permissionStatus',
+            ),
+            OutlinedButton(
+              onPressed: () {
+                requestPermission(_permission);
+              },
+              child: const Text('GET PERMISSION'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
